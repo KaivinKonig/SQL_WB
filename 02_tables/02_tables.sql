359d42b5ce29b053114184fa03c4cb4cfcc3973d
KaivinKonig <konigsmannkaivin@gmail.com>
2025-08-11 12:20:07 +0200

    update

--- 02_tables/02_tables.sql
+++ 02_tables/02_tables.sql
@@ -16,6 +16,7 @@ DROP TABLE IF EXISTS kunde;
 -- Tabelle anlegen
 CREATE TABLE IF NOT EXISTS kunde
 (
+    k_id INT AUTO_INCREMENT PRIMARY KEY,
     k_name VARCHAR(45),
     k_alter TINYINT
 );
@@ -27,8 +28,11 @@ SHOW TABLES;
 DESCRIBE kunde;
 
 -- Datensätze/Zeilen
-INSERT INTO kunde(k_name,k_alter) VALUES("Meier", 28);
-INSERT INTO kunde(k_name,k_alter) VALUES("Hussein", 32);
+INSERT INTO kunde(k_id,k_name,k_alter) VALUES(DEFAULT,"Meier", 28);
+INSERT INTO kunde(k_id,k_name,k_alter) VALUES(DEFAULT,"Hussein", 32);
+INSERT INTO kunde(k_id,k_name,k_alter) VALUES(DEFAULT,"Li", 26);
+INSERT INTO kunde(k_id,k_name,k_alter) VALUES(DEFAULT,"Li", 40);
+INSERT INTO kunde(k_name,k_alter) VALUES("Li", 61);
 
 -- Tabellendaten (Inhalte) anzeigen
 SELECT * FROM kunde;
