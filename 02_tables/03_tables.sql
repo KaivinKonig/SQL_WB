2d05cc3aa4ade3187c3937f08b6da487f6a51ac1
KaivinKonig <konigsmannkaivin@gmail.com>
2025-08-11 12:39:37 +0200

    update

---
+++ 02_tables/03_tables.sql
@@ -0,0 +1,36 @@
+\! cls
+
+-- UNIQUE
+
+-- DB anlegen, falls noch nicht existent
+CREATE DATABASE IF NOT EXISTS boo;
+
+-- DB wechseln
+USE boo;
+
+-- Tabellte löschen, falls existent
+-- Nur bei Test / Entwicklung
+DROP TABLE IF EXISTS kunde;
+
+-- Tabelle anlegen
+CREATE TABLE IF NOT EXISTS kunde
+(
+    k_id INT AUTO_INCREMENT PRIMARY KEY,
+    k_name VARCHAR(45),
+    k_alter TINYINT,
+    k_mail VARCHAR(100) UNIQUE
+);
+
+
+-- Struktur anzeigen
+DESCRIBE kunde;
+
+-- Datensätze/Zeilen
+-- INSERT INTO kunde(k_id,k_name,k_alter) VALUES(DEFAULT,"Meier", 28);
+-- INSERT INTO kunde(k_id,k_name,k_alter) VALUES(DEFAULT,"Hussein", 32);
+-- INSERT INTO kunde(k_id,k_name,k_alter) VALUES(DEFAULT,"Li", 26);
+-- INSERT INTO kunde(k_id,k_name,k_alter) VALUES(DEFAULT,"Li", 40);
+-- INSERT INTO kunde(k_name,k_alter) VALUES("Li", 61);
+
+-- Tabellendaten (Inhalte) anzeigen
+SELECT * FROM kunde;
