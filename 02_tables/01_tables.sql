686913c263af027eb29518fd8753162deab1250b
KaivinKonig <konigsmannkaivin@gmail.com>
2025-08-11 10:59:38 +0200

    01_table DROP/DESCRIBE

--- 02_tables/01_tables.sql
+++ 02_tables/01_tables.sql
@@ -6,6 +6,23 @@ SHOW DATABASES;
 -- DB anlegen, falls noch nicht existent
 CREATE DATABASE IF NOT EXISTS boo;
 
--- DBs anzeigen
-SHOW DATABASES;
+-- DB wechseln
+USE boo;
+
+-- Tabellte löschen, falls existent
+-- Nur bei Test / Entwicklung
+DROP TABLE IF EXISTS kunde;
+
+-- Tabelle anlegen
+CREATE TABLE IF NOT EXISTS kunde
+(
+    k_name VARCHAR(45),
+    k_alter TINYINT
+);
+
+-- Tabellen anzeigen
+SHOW TABLES;
+
+-- Struktur anzeigen
+DESCRIBE kunde;
 
