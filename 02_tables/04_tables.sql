a396ed12851c0566f96e294cf662540072e3af04
KaivinKonig <konigsmannkaivin@gmail.com>
2025-08-12 11:34:28 +0200

    Update

--- 02_tables/04_tables.sql
+++ 02_tables/04_tables.sql
@@ -14,7 +14,7 @@ DROP TABLE IF EXISTS kunde;
 CREATE TABLE IF NOT EXISTS kunde
 (
     k_id INT AUTO_INCREMENT PRIMARY KEY,
-    k_name VARCHAR(45) NOT NULL DEFAULT "TBA",
+    k_name VARCHAR(45) NOT NULL DEFAULT 'TBA' ,
     k_alter TINYINT,
     k_mail VARCHAR(100) UNIQUE,
     k_pw VARCHAR(100)
@@ -24,12 +24,12 @@ CREATE TABLE IF NOT EXISTS kunde
 -- Struktur anzeigen
 DESCRIBE kunde;
 
--- Datensätze/Zeilen
-INSERT INTO kunde(k_name,k_alter,k_mail,k_pw) VALUES("Meier", 28, "meier@web.de", 'maier1234');
-INSERT INTO kunde(k_name,k_alter,k_mail,k_pw) VALUES("Hussein", 32, "hussein@web.de", 'hus#123');
-INSERT INTO kunde(k_name,k_alter,k_mail,k_pw) VALUES("Li", 26, "li@web.de", 'lilalu');
-INSERT INTO kunde(k_name,k_alter,k_mail,k_pw) VALUES("Li", 40, "li2@web.de", 'wauwau');
-INSERT INTO kunde(k_name,k_alter,k_mail,k_pw) VALUES("Li", 61, "li_shanghai@web.de", 'shanghai');
+-- Datensätze/Zeilen / PW IMMER HASHEN!! --> SHA2 ('pw' ,256)
+INSERT INTO kunde(k_name,k_alter,k_mail,k_pw) VALUES('Meier', 28, 'meier@web.de', SHA2('maier1234',256));
+INSERT INTO kunde(k_name,k_alter,k_mail,k_pw) VALUES('Hussein', 32, 'hussein@web.de', SHA2('hus#!12',256));
+INSERT INTO kunde(k_name,k_alter,k_mail,k_pw) VALUES('Li', 26, 'li@web.de', SHA2('li1234',256));
+INSERT INTO kunde(k_name,k_alter,k_mail,k_pw) VALUES('Li', 40, 'li2@web.de', SHA2('#!aBcD',256));
+INSERT INTO kunde(k_name,k_alter,k_mail,k_pw) VALUES('Li', 61, 'li_shanghai@web.de', SHA2('wau12',256));
 
 
 -- Tabellendaten (Inhalte) anzeigen
