60318a3bbe073e183eba20726d05b6599d2d709c
KaivinKonig <konigsmannkaivin@gmail.com>
2025-08-14 09:14:32 +0200

    update

--- 03_select.sql/02_select.sql
+++ 03_select.sql/02_select.sql
@@ -1,7 +1,47 @@
 \! cls
 
--- 02 SELECT
-
-SELECT email FROM boo.contacts;
+--  02 SELECT
 
 
+-- SELECT 
+--     *
+-- FROM languages.contacts
+-- ;
+
+
+-- SELECT 
+--     id,
+--     email
+-- FROM languages.contacts
+-- ;
+
+# Scharfe Suche
+-- SELECT 
+--     id,
+--     email,
+--     city
+-- FROM languages.contacts
+-- WHERE city = 'Berlin'
+-- ;
+
+# unscharfe Suche
+-- SELECT 
+--     id,
+--     email,
+--     city
+-- FROM languages.contacts
+-- -- WHERE city LIKE 'Fra%'
+-- WHERE city LIKE '%furt'
+-- ;
+
+# Sortierung
+SELECT 
+    name AS Name,
+    email AS eMail,
+    city AS Stadt
+FROM languages.contacts
+-- WHERE city LIKE 'Fra%'
+WHERE city LIKE '%furt'
+-- ORDER BY name ASC
+ORDER BY name DESC
+;
